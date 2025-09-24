00c65c0542d8ef5da353b9af0a617c14a8913a5a
midesweb <malvarez@desarrolloweb.com>
2025-09-24 13:04:28 +0200

    corregir declaración e introducir push()

--- 19-parametros-predeterminados.js
+++ 19-parametros-predeterminados.js
@@ -10,12 +10,12 @@ let valores = [67, 8, -7, 32, 9, -1, 12];
 function valoresSuperiores(umbral, valores = []) {
   console.log('valores: ', valores, 'umbral:', umbral);
   let superiores = [];
-  for(valorActual of valores) {
+  for(let valorActual of valores) {
     if(valorActual > umbral) {
-      superiores[superiores.length] = valorActual;
+      superiores.push(valorActual);
     }
   }
   return superiores;
 }
 
-console.log(valoresSuperiores(10));
+console.log(valoresSuperiores(10, valores));
